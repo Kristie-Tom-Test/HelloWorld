f826b1caabafdffec3dc45a08e41d7021c68db41
TomKristie <65685417+TomKristie@users.noreply.github.com>
2020-07-02 17:01:01 -0400

    Third-Party Changes

---
+++ src/handle-github/fork.ts
@@ -0,0 +1,46 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {GitHubContext, Logger} from '../types';
+import {Octokit} from '@octokit/rest';
+
+/**
+ * Fork the GitHub owner's repository
+ * If fork already exists no new fork is created and no error occurs
+ * @param gitHubContext The configuration for interacting with GitHub
+ * @param logger The logger instance
+ * @param octokit The authenticated octokit instance
+ * @returns the forked repository name, as well as the owner of that fork
+ */
+async function fork(
+  gitHubContext: GitHubContext,
+  logger: Logger,
+  octokit: Octokit
+) {
+  const forkedRepo = (
+    await octokit.repos.createFork({
+      owner: gitHubContext.mainOwner,
+      repo: gitHubContext.mainRepo,
+    })
+  ).data;
+  console.log(forkedRepo);
+  logger.debug('Fork was either successful, or fork already exists');
+  // TODO autosync
+  return {
+    newRepo: forkedRepo.name,
+    newOwner: forkedRepo.owner.login,
+  };
+}
+
+export {fork};
---
+++ src/handle-github/pr.ts
@@ -0,0 +1,51 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {GitHubContext, Logger} from '../types';
+import {Octokit} from '@octokit/rest';
+
+/**
+ * Create a GitHub PR on the main organization's repo
+ * @param gitHubContext The configuration for interacting with GitHub
+ * @param logger The logger instance
+ * @param octokit The authenticated octokit instance
+ * @param workerBranchName the branch name that contains the changes
+ */
+async function makePR(
+  gitHubContext: GitHubContext,
+  logger: Logger,
+  octokit: Octokit,
+  workerBranchName: string
+) {
+  // TODO - handle when there is no fork instance
+  if (!gitHubContext.workerRepo) {
+    logger.error('PR is not made because there is no working repo');
+    return;
+  }
+  const isForkOutOfDate = false;
+  if (isForkOutOfDate) {
+    logger.debug('Fork is out of synch');
+    // TODO - autosync fork, update branch, and re-apply changes
+  }
+  const x = (await octokit.pulls.create({
+    owner: gitHubContext.mainOwner,
+    repo: gitHubContext.workerRepo,
+    title: gitHubContext.prTitle,
+    head: `${gitHubContext.workerOwner}:${workerBranchName}`,
+    base: 'master',
+  })).data;
+  console.log(x)
+}
+
+export {makePR};
